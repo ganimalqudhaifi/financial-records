a9829b9d6a8c1d0ec4c3686feed3360b7f68e4ff
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-02-23 22:01:44 +0700

    style(FinancialRecordsActionDelete): change content with icon and rounded button

--- components/molecules/Modal/index.js
+++ components/molecules/Modal/index.js
@@ -6,7 +6,7 @@ import styles from './Modal.module.css';
 export default function Modal({ action, children, style }) {
   const { dispatch } = useContext(RootContext);
   return (
-    <div id={action} className="fixed top-0 left-0 hidden z-50 w-full h-full bg-black/40" onClick={(e) => dispatch(closeModal(e, action))}>
+    <div id={action} className="fixed top-0 left-0 hidden z-50 p-4 w-full h-full bg-black/40" onClick={(e) => dispatch(closeModal(e, action))}>
       <div className={`relative mx-auto rounded-lg bg-white ${styles[style]} animate-pop`}>
         <span className="absolute top-px right-3.5 font-bold text-3xl md:text-4xl cursor-pointer" onClick={() => dispatch(hideModal(action))}>&times;</span>
         {children}
--- components/templates/FinancialRecordsApp/FinancialRecordsActionDelete.js
+++ components/templates/FinancialRecordsApp/FinancialRecordsActionDelete.js
@@ -12,10 +12,10 @@ export default function FinancialRecordsActionDelete({ id }) {
     <>
       <button className="py-1 px-2 mx-1 border-slate-900 border-2 rounded" onClick={() => dispatch(showModal(action))}>Hapus</button>
       <Modal style="modal-content-delete" action={action}>
-        <p className="mt-4 text-3xl md:text-4xl lg:text-5xl text-center">Hapus Catatan</p>
-        <p className="px-3 my-7 md:my-10 lg:my-14 text-base md:text-lg lg:text-xl whitespace-normal">Apakah anda yakin ingin menghapus catatan?</p>
-        <button className="py-2 md:py-3 lg:py-4 w-1/2 text-lg md:text-xl lg:text-2xl text-slate-900 bg-slate-300/80 hover:bg-slate-400/80" onClick={() => dispatch(hideModal(action))}>Batal</button>
-        <button className="py-2 md:py-3 lg:py-4 w-1/2 text-lg md:text-xl lg:text-2xl text-slate-50 bg-red-500/80 hover:bg-red-500 " onClick={() => (!isDemo ? deleteRecord(isDemo, id) : dispatch(deleteRecord(isDemo, id)))}>Hapus</button>
+        <svg aria-hidden="true" className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <h3 className="px-3 mb-5 text-lg lg:text-xl text-gray-500 whitespace-normal">Apakah anda yakin ingin menghapus catatan?</h3>
+        <button className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm lg:text-lg inline-flex items-center px-5 py-2.5 text-center mr-2 lg:mr-3" onClick={() => (!isDemo ? deleteRecord(isDemo, id) : dispatch(deleteRecord(isDemo, id)))}>Hapus</button>
+        <button className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm lg:text-lg font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" onClick={() => dispatch(hideModal(action))}>Batal</button>
       </Modal>
     </>
   );

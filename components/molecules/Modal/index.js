a3ce93bdf6e363e98b9a89f229905773f44fc2f6
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-02-23 21:33:55 +0700

    style(modal): change shape to rounded

--- components/molecules/Modal/index.js
+++ components/molecules/Modal/index.js
@@ -6,9 +6,9 @@ import styles from './Modal.module.css';
 export default function Modal({ action, children, style }) {
   const { dispatch } = useContext(RootContext);
   return (
-    <div id={action} className={styles.modal} onClick={(e) => dispatch(closeModal(e, action))}>
-      <div className={`${styles[style]} animate-pop`}>
-        <span className={styles.close} onClick={() => dispatch(hideModal(action))}>&times;</span>
+    <div id={action} className="fixed top-0 left-0 hidden z-50 w-full h-full bg-black/40" onClick={(e) => dispatch(closeModal(e, action))}>
+      <div className={`relative mx-auto rounded-lg bg-white ${styles[style]} animate-pop`}>
+        <span className="absolute top-px right-3.5 font-bold text-3xl md:text-4xl cursor-pointer" onClick={() => dispatch(hideModal(action))}>&times;</span>
         {children}
       </div>
     </div>

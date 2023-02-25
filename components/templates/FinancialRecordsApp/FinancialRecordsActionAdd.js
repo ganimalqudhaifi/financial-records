f5634ec239e28951a22a5a2a330b2e68db718341
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-02-25 23:38:53 +0700

    style(FinancialRecordsActionAdd): change content within modal box and remove delete button

--- components/templates/FinancialRecordsApp/FinancialRecordsActionAdd.js
+++ components/templates/FinancialRecordsApp/FinancialRecordsActionAdd.js
@@ -62,62 +62,62 @@ export default function FinancialRecordsActionAdd() {
       <button className="py-1.5 md:py-2 px-3 md:px-4 bg-slate-800 text-slate-50 rounded-[3px]" onClick={() => dispatch(showModal(action))}>Tambah</button>
 
       <Modal style="modal-content-edit" action={action}>
-        <p className="mt-4 text-3xl md:text-4xl lg:text-5xl text-center mb-2.5 my-2">Menambah Catatan</p>
-        <form onSubmit={handleSubmit}>
-          <label className="font-normal pl-0.5 capitalize text-left text-base mb-0.5 block" htmlFor="jumlah">Jumlah</label>
-          <input
-            className="w-full p-2 border-[1px] border-slate-900/40 rounded mb-4 text-sm"
-            type="number"
-            id="jumlah"
-            name="jumlah"
-            value={inputs.jumlah || ''}
-            placeholder="jumlah"
-            step="10"
-            onChange={handleChange}
-            required
-          />
-
-          <label className="font-normal pl-0.5 capitalize text-left text-base mb-0.5 block" htmlFor="keterangan">Keterangan</label>
-          <input
-            className="w-full p-2 border-[1px] border-slate-900/40 rounded mb-4 text-sm"
-            type="text"
-            id="keterangan"
-            name="keterangan"
-            value={inputs.keterangan || ''}
-            placeholder="keterangan"
-            onChange={handleChange}
-            required
-          />
-
-          <label className="font-normal pl-0.5 capitalize text-left text-base mb-0.5 block" htmlFor="jenis">Jenis</label>
-          <select
-            className="w-full p-2 border-[1px] border-slate-900/40 rounded mb-4 text-sm"
-            id="jenis"
-            name="jenis"
-            value={inputs.jenis || 'Penerimaan'}
-            onChange={handleChange}
-          >
-            <option>Penerimaan</option>
-            <option>Pengeluaran</option>
-          </select>
-
-          <label className="font-normal pl-0.5 capitalize text-left text-base mb-0.5 block" htmlFor="date">Tanggal</label>
-          <input
-            className="w-full p-2 border-[1px] border-slate-900/40 rounded mb-4 text-sm"
-            type="date"
-            id="date"
-            name="tanggal"
-            value={inputs.tanggal || ''}
-            placeholder="Select date"
-            onChange={handleChange}
-            required
-          />
-
-          <div className="mt-2">
-            <button type="button" className="py-2 md:py-3 lg:py-4 w-1/2 text-lg md:text-xl lg:text-2xl text-slate-900 bg-slate-300/80 hover:bg-slate-400/80" onClick={() => dispatch(hideModal(action))}>Batal</button>
-            <button className="py-2 md:py-3 lg:py-4 w-1/2 text-lg md:text-xl lg:text-2xl text-slate-50 bg-slate-800/80 hover:bg-slate-800" type="submit">Kirim</button>
+        <h3 className="mb-4 text-xl text-left font-medium text-gray-900 dark:text-white">Membuat Catatan Baru</h3>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label className="block mb-1.5 text-sm font-medium text-gray-900 dark:text-white" htmlFor="jumlah">Jumlah</label>
+            <input
+              className="block p-2.5 w-full text-sm text-slate-900 bg-slate-50 border border-slate-400 rounded-lg focus:outline-slate-500"
+              type="number"
+              id="jumlah"
+              name="jumlah"
+              value={inputs.jumlah || ''}
+              placeholder="jumlah"
+              step="10"
+              onChange={handleChange}
+              required
+            />
           </div>
-
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white" htmlFor="keterangan">Keterangan</label>
+            <input
+              className="block p-2.5 w-full text-sm text-slate-900 bg-slate-50 border border-slate-400 rounded-lg focus:outline-slate-500"
+              type="text"
+              id="keterangan"
+              name="keterangan"
+              value={inputs.keterangan || ''}
+              placeholder="keterangan"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white" htmlFor="jenis">Jenis</label>
+            <select
+              className="block p-2.5 w-full text-sm text-slate-900 bg-slate-50 border border-slate-400 rounded-lg focus:outline-slate-500"
+              id="jenis"
+              name="jenis"
+              value={inputs.jenis || 'Penerimaan'}
+              onChange={handleChange}
+            >
+              <option>Penerimaan</option>
+              <option>Pengeluaran</option>
+            </select>
+          </div>
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white" htmlFor="date">Tanggal</label>
+            <input
+              className="block p-2.5 w-full text-sm text-slate-900 bg-slate-50 border border-slate-400 rounded-lg focus:outline-slate-500"
+              type="date"
+              id="date"
+              name="tanggal"
+              value={inputs.tanggal || ''}
+              placeholder="Select date"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button className="py-2.5 w-full font-medium text-lg text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 rounded-lg" type="submit">Kirim</button>
         </form>
       </Modal>
     </>

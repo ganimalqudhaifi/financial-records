731d60b5ea6614e1ff304db098227ef4730011b2
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-03-17 16:28:28 +0700

    fix(AppLayout): overflow breakpoints at small devices

--- components/layout/AppLayout/index.jsx
+++ components/layout/AppLayout/index.jsx
@@ -2,7 +2,7 @@ import { AppSidebar, Footers } from '../../organisms';
 
 export default function AppLayout({ children, ...rest }) {
   return (
-    <div className="flex flex-col w-full h-full bg-gray-300/60">
+    <div className="flex flex-col w-full h-full overflow-auto bg-gray-300/60">
       <div className="lg:flex flex-1 w-full">
         <AppSidebar {...rest} />
         {children}

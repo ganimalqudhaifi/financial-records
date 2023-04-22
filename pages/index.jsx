e9ee033e9b6473f95ce745c5b918787bf0861d0e
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-04-22 18:16:34 +0700

    feat(home_page): add about and contact section

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,7 +22,7 @@ export default function Home() {
         <title>Financial Records - Home</title>
       </Head>
 
-      <div className="flex flex-col px-4 sm:px-[5%] md:px-[6%] lg:px-[8%] w-full min-h-[100vh] bg-bg-color text-text-color">
+      <div className="flex flex-col px-4 sm:px-[5%] md:px-[6%] lg:px-[8%] w-full min-h-[100vh] bg-bg-color text-text-color scroll-smooth">
         <header className="sticky flex flex-wrap justify-between py-6 duration-500 items-center">
           <Link href="/" className="flex">
             <svg className="fill-main-color mr-2.5 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 7V5c0-1.103-.897-2-2-2H5C3.346 3 2 4.346 2 6v12c0 2.201 1.794 3 3 3h15c1.103 0 2-.897 2-2V9c0-1.103-.897-2-2-2zm-2 9h-2v-4h2v4zM5 7a1.001 1.001 0 0 1 0-2h13v2H5z" /></svg>
@@ -96,14 +96,14 @@ export default function Home() {
           <nav className="lg:order-1 w-full lg:w-auto h-0 lg:h-auto">
             <ul className="w-full mt-4 lg:mt-0 rounded-lg overflow-hidden lg:flex ">
               <li className={`${!isActive ? 'opacity-0 invisible' : 'opacity-100 visible'}duration-500 lg:opacity-100 lg:visible last:lg:hidden last:border-t-2 last:border-gray-500 active:brightness-200 active:duration-[0ms]`}><Link href="#" className="block px-5 py-[10px] text-gray-200 font-medium duration-200 bg-gray-700 hover:bg-gray-600 hover:duration-[0ms] hover:text-main-color lg:bg-transparent lg:px-6 lg:hover:bg-transparent lg:duration-[0ms]">Home</Link></li>
-              <li className={`${!isActive ? 'opacity-0 invisible' : 'opacity-100 visible'}duration-500 lg:opacity-100 lg:visible last:lg:hidden last:border-t-2 last:border-gray-500 active:brightness-200 active:duration-[0ms]`}><Link href="#" className="block px-5 py-[10px] text-gray-200 font-medium duration-200 bg-gray-700 hover:bg-gray-600 hover:duration-[0ms] hover:text-main-color lg:bg-transparent lg:px-6 lg:hover:bg-transparent lg:duration-[0ms]">About</Link></li>
-              <li className={`${!isActive ? 'opacity-0 invisible' : 'opacity-100 visible'}duration-500 lg:opacity-100 lg:visible last:lg:hidden last:border-t-2 last:border-gray-500 active:brightness-200 active:duration-[0ms]`}><Link href="#" className="block px-5 py-[10px] text-gray-200 font-medium duration-200 bg-gray-700 hover:bg-gray-600 hover:duration-[0ms] hover:text-main-color lg:bg-transparent lg:px-6 lg:hover:bg-transparent lg:duration-[0ms]">Contact</Link></li>
+              <li className={`${!isActive ? 'opacity-0 invisible' : 'opacity-100 visible'}duration-500 lg:opacity-100 lg:visible last:lg:hidden last:border-t-2 last:border-gray-500 active:brightness-200 active:duration-[0ms]`}><Link href="#About" className="block px-5 py-[10px] text-gray-200 font-medium duration-200 bg-gray-700 hover:bg-gray-600 hover:duration-[0ms] hover:text-main-color lg:bg-transparent lg:px-6 lg:hover:bg-transparent lg:duration-[0ms]">About</Link></li>
+              <li className={`${!isActive ? 'opacity-0 invisible' : 'opacity-100 visible'}duration-500 lg:opacity-100 lg:visible last:lg:hidden last:border-t-2 last:border-gray-500 active:brightness-200 active:duration-[0ms]`}><Link href="#Contact" className="block px-5 py-[10px] text-gray-200 font-medium duration-200 bg-gray-700 hover:bg-gray-600 hover:duration-[0ms] hover:text-main-color lg:bg-transparent lg:px-6 lg:hover:bg-transparent lg:duration-[0ms]">Contact</Link></li>
               <li className={`${!isActive ? 'opacity-0 invisible' : 'opacity-100 visible'}duration-500 lg:opacity-100 lg:visible last:lg:hidden last:border-t-2 last:border-gray-500 active:brightness-200 active:duration-[0ms]`}><Link href="/register" className="block px-5 py-[10px] text-gray-200 font-medium duration-200 bg-gray-700 hover:bg-gray-600 hover:duration-[0ms] hover:text-main-color lg:bg-transparent lg:px-6 lg:hover:bg-transparent lg:duration-[0ms]">Register</Link></li>
             </ul>
           </nav>
         </header>
 
-        <main className="mt-10 flex-1">
+        <main className="mt-10 flex-1 flex flex-col gap-y-10">
           <article className="px-6 md:px-12 grid sm:grid-cols-2">
             <section className="my-auto">
               <h1 className="text-5xl text-main-color font-bold">Financial Records.</h1>
@@ -155,13 +155,63 @@ export default function Home() {
               </svg>
             </section>
           </article>
-        </main>
 
-        <div>
-          - TODO!: add about section
-          <br />
-          - TODO!: add contact section
-        </div>
+          <article id="About" className="px-6 md:px-12 min-h-screen flex flex-col items-center">
+            <section className="my-auto grid grid-cols-2 gap-x-8">
+              <h1 className="pb-10 text-5xl text-main-color font-bold text-center col-span-2">About Me</h1>
+              <p className="text-md mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eligendi nostrum amet explicabo labore, quis quam vel ducimus voluptatem quos fugiat. Mollitia saepe pariatur deleniti? Architecto mollitia nulla incidunt quidem.</p>
+              <p className="text-md mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eligendi nostrum amet explicabo labore, quis quam vel ducimus voluptatem quos fugiat. Mollitia saepe pariatur deleniti? Architecto mollitia nulla incidunt quidem.</p>
+            </section>
+          </article>
+
+          <article className="min-h-full">
+            <section id="Contact" className="py-6 text-gray-50">
+              <h1 className="pb-10 text-5xl text-main-color font-bold text-center col-span-2">Contact</h1>
+              <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
+                <div className="py-6 md:py-0 md:px-6">
+                  <h1 className="text-4xl font-bold">Get in touch</h1>
+                  <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
+                  <div className="space-y-4">
+                    <p className="flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                        <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                      </svg>
+                      <span>Depok, 16411 Jawa Barat</span>
+                    </p>
+                    <p className="flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                        <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                      </svg>
+                      <span>+62853456789XX</span>
+                    </p>
+                    <p className="flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                      </svg>
+                      <span>contact@business.com</span>
+                    </p>
+                  </div>
+                </div>
+                <form noValidate="" className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
+                  <label className="block">
+                    <span className="mb-1">Full name</span>
+                    <input type="text" placeholder="Leroy Jenkins" className="block p-2 w-full rounded-md shadow-sm border border-gray-300 focus:ring focus:ring-opacity-75 focus:gray-100 bg-bg-color" />
+                  </label>
+                  <label className="block">
+                    <span className="mb-1">Email address</span>
+                    <input type="email" placeholder="leroy@jenkins.com" className="block p-2 w-full rounded-md shadow-sm border border-gray-300 focus:ring focus:ring-opacity-75 focus:gray-100 bg-bg-color" />
+                  </label>
+                  <label className="block">
+                    <span className="mb-1">Message</span>
+                    <textarea rows="3" className="block p-2 w-full rounded-md text-gray-400 border border-gray-300 focus:ring focus:ring-opacity-75 focus:gray-100 bg-bg-color" />
+                  </label>
+                  <button type="button" className="self-center px-8 py-3 text-lg rounded  focus:ring hover:ring focus:ring-opacity-75 bg-main-color text-bg-color focus:ring-purple-400 hover:ring-purple-400">Submit</button>
+                </form>
+              </div>
+            </section>
+          </article>
+        </main>
 
         <Script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js" />
         <Script noModule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js" />

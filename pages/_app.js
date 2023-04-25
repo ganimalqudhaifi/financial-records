4e130871bcf9c3fefd90d2dad480d2a48ebb21d0
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-04-25 09:59:49 +0700

    feat(dashboard_page): add personal information section

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,12 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <GlobalContextProvider>
-      <Component {...pageProps} />
-    </GlobalContextProvider>
+    <>
+      <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+      <GlobalContextProvider>
+        <Component {...pageProps} />
+      </GlobalContextProvider>
+    </>
   );
 }
 
--- pages/app/profile/index.jsx
+++ pages/app/profile/index.jsx
@@ -7,6 +7,9 @@ import checkUID from '../../../utils/checkUID';
 export default function App() {
   const [isLogin, setIsLogin] = useState(false);
   const [user, setUser] = useState({});
+  const [edits, setEdits] = useState({
+    personalInformation: false,
+  });
 
   const router = useRouter();
 
@@ -30,11 +33,52 @@ export default function App() {
             <div>
               - TODO!: add feature for edit profile picture / avatar
               <br />
-              - TODO!: add feature for edit username
-              <br />
               - TODO!: add feature for change password - it would be verified email to unlock this feature
               <br />
               - TODO!: add feature add link to social media - it will be direct link with icon on sidebar
+            </div>
+            <div className="p-5 w-full bg-white rounded">
+              <h3 className="font-medium text-xl">Personal Information</h3>
+              <form className="mt-5 grid grid-cols-3">
+                <div className="grid grid-cols-2 col-span-2 gap-x-5 gap-y-3">
+                  <div className="flex flex-col">
+                    <label htmlFor="firstName" className="mb-1 text-sm text-gray-600">First Name</label>
+                    <input id="firstName" type="text" placeholder="firstName" className={`${edits.personalInformation && 'p-1 border-2 border-slate-600 rounded'}`} disabled={!edits.personalInformation} />
+                  </div>
+                  <div className="flex flex-col ">
+                    <label htmlFor="firstName" className="mb-1 text-sm text-gray-600">Last Name</label>
+                    <input id="firstName" type="text" placeholder="lastName" className={`${edits.personalInformation && 'p-1 border-2 border-slate-600 rounded'}`} disabled={!edits.personalInformation} />
+                  </div>
+                  <div className="flex flex-col ">
+                    <label htmlFor="firstName" className="mb-1 text-sm text-gray-600">Email Address</label>
+                    <input id="firstName" type="text" placeholder="emailAddress" className={`${edits.personalInformation && 'p-1 border-2 border-slate-600 rounded'}`} disabled={!edits.personalInformation} />
+                  </div>
+                  <div className="flex flex-col ">
+                    <label htmlFor="firstName" className="mb-1 text-sm text-gray-600">Phone</label>
+                    <input id="firstName" type="text" placeholder="-" className={`${edits.personalInformation && 'p-1 border-2 border-slate-600 rounded'}`} disabled={!edits.personalInformation} />
+                  </div>
+                  <div className="flex flex-col ">
+                    <label htmlFor="firstName" className="mb-1 text-sm text-gray-600">Bio</label>
+                    <input id="firstName" type="text" placeholder="Bio" className={`${edits.personalInformation && 'p-1 border-2 border-slate-600 rounded'}`} disabled={!edits.personalInformation} />
+                  </div>
+                </div>
+                <div className="px-5 py-1 grid justify-end items-start">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setEdits({
+                        personalInformation: !edits.personalInformation,
+                      });
+                    }}
+                    className="grid grid-flow-col px-2 py-1 border justify-center items-center border-slate-300 hover:border-slate-500 active:border-slate-400 text-slate-600 hover:text-slate-700 active:text-slate-600 rounded-full"
+                  >
+                    <p className="text-md">
+                      {edits.personalInformation ? 'Save' : 'Edit'}
+                    </p>
+                    {edits.personalInformation ? <i className="ml-1 bx bx-save text-base " /> : <i className="ml-1 bx bx-edit-alt text-base " />}
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </AppLayout>

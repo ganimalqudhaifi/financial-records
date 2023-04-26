806395c67f233beeb132ddc23eb8f6c7ff498bba
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-04-27 04:03:45 +0700

    style(FinancialRecords): adjust min width on small device

--- components/templates/FinancialRecordsApp/FinancialRecords.js
+++ components/templates/FinancialRecordsApp/FinancialRecords.js
@@ -8,7 +8,7 @@ import FinancialRecordsPaginantion from './FinancialRecordsPagination';
 export default function FinancialRecords() {
   return (
     <div className="w-full min-w-fit bg-gray-50/90 p-4 rounded">
-      <div className="flex justify-between flex-wrap px-0.5 mb-1">
+      <div className="flex justify-between min-w-max flex-wrap px-0.5 mb-1">
         <div className="space-x-2">
           <span>
             <FinancialRecordsActionAdd />

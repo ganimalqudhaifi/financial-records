f6f3a10ec5b31344e0724839d036cf564cf72fdf
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-09-02 16:39:21 +0700

    clean code demo-pages

--- pages/demo/index.jsx
+++ pages/demo/index.jsx
@@ -1,13 +1,11 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
 import { AppLayout, FinancialRecords } from '../../components';
-import { database, ref, set } from '../../config/firebase';
-import { globalActionType, useGlobalContext } from '../../context';
+import { useGlobalContext } from '../../context';
 import { getData } from '../../utils/data';
-import { storage } from '../../utils';
 
 export default function Demo({ records }) {
-  const { state, dispatch, changeIsDemoState, changeRecordsState } = useGlobalContext();
+  const { state, dispatch, changeIsDemoState, changeRecordsState, changeSaldoAwalState } = useGlobalContext();
   const { isDemo } = state;
   const user = {
     displayName: 'Demo',
@@ -15,18 +13,9 @@ export default function Demo({ records }) {
   };
 
   useEffect(() => {
-    const uid = storage.getUID();
-
-    const changeSaldoAwal = (isDemo, payload) => {
-      if (!isDemo) {
-        set(ref(database, `users/${uid}/saldoAwal`), payload);
-      }
-      dispatch({ type: globalActionType.CHANGE_SALDO_AWAL, payload });
-    };
-
     changeIsDemoState(true);
     changeRecordsState(records);
-    changeSaldoAwal(isDemo, 0);
+    changeSaldoAwalState(0);
 
     return () => {
       changeIsDemoState(false);

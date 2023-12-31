0afaba68b53382ee7c5ecfa0e93a978f58f7191b
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2023-12-31 19:42:18 +0700

    chore: update firebase config environment and revoke new token

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,6 +5,7 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
@@ -13,7 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
-export const database = getDatabase(app, 'https://financial-records-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/');
-// export * from 'firebase/database';
-// export * from 'firebase/auth';
+export const database = getDatabase(app);

332db39b09da517452f312f86eed632bbed5d8a4
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2024-08-04 03:15:33 +0700

    Remove redundant code from utils

--- utils/authentication.ts
+++
@@ -1,9 +0,0 @@
-import { signOut } from "firebase/auth";
-import { auth } from "@/lib/firebase/auth";
-import { alertToast } from "./sweetAlert";
-
-export const userSignOut = () => {
-  signOut(auth).catch((error) => {
-    alertToast(error.message);
-  });
-};

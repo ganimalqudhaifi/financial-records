c8317d9d2b6d4c01e29596303e7a366e109460c7
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2024-08-08 14:41:07 +0700

    Fix check condition of selectedAccount in AccountsDropdown

--- components/App/AccountsDropdown.tsx
+++ components/App/AccountsDropdown.tsx
@@ -14,10 +14,10 @@ export default function AccountsDropdown() {
   const [isActive, setIsActive] = useState(true);
 
   useEffect(() => {
-    if (accounts.length && selectedAccount === null) {
+    if (accounts.length && Object.keys(selectedAccount).length === 0) {
       dispatch(selectAccount(accounts[0]));
     }
-  }, []);
+  }, [accounts, dispatch, selectedAccount]);
 
   const handleSelectAccount = (account: Account) => {
     setIsActive(!isActive);
@@ -31,7 +31,7 @@ export default function AccountsDropdown() {
         className={`group peer ${isActive && "is-active"} inline-flex justify-between items-center w-full p-2 rounded-lg text-white font-medium hover:bg-slate-800`}
         onClick={() => setIsActive(!isActive)}
       >
-        {selectedAccount.name ? <p>{selectedAccount.name}</p> : <p>...</p>}
+        {selectedAccount?.name ? <p>{selectedAccount.name}</p> : <p>...</p>}
         <IoChevronDownOutline className="ml-2.5 group-[.is-active]:-rotate-90 duration-300" />
       </button>
       <div className="overflow-hidden max-h-72 duration-500 peer-[.is-active]:max-h-0 peer-[.is-active]:duration-200">

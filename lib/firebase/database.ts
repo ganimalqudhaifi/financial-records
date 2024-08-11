5bcdd9c5a2f7aa3fb37da29cbf6605583b48c763
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2024-08-11 22:35:31 +0700

    Update naming conventions in firebase database

--- lib/firebase/database.ts
+++ lib/firebase/database.ts
@@ -1,42 +1,44 @@
 import { getDatabase, push, ref, remove, set } from "firebase/database";
 import { Account, Record } from "@/types";
-import { selectUser } from "../redux/features/user/userSlice";
-import { store } from "../redux/store";
 import { app } from "./client";
 
 export const database = getDatabase(app);
 
-const getCurrentUserUid = (): string | undefined => {
-  const { user } = selectUser(store.getState());
-  return user?.uid;
+const getCurrentUserUid = async () => {
+  const res = await fetch("/api/user", {
+    method: "GET",
+  });
+  const user = await res.json();
+  return user.uid;
 };
 
-export const addAccount = (uid: string, payload: Account) => {
+export const firebaseAddAccount = (uid: string, payload: Account) => {
   const accountsRef = ref(database, `users/${uid}/accounts`);
   push(accountsRef, payload);
 };
 
-export const updateAccount = (uid: string, payload: Account) => {
+export const firebaseUpdateAccount = (uid: string, payload: Account) => {
   const accountRef = ref(database, `users/${uid}/accounts/${payload.id}`);
   set(accountRef, payload);
 };
 
-export const deleteAccount = (uid: string, payload: Account) => {
+export const firebaseDeleteAccount = (uid: string, payload: Account) => {
   const accountRef = ref(database, `users/${uid}/accounts/${payload.id}`);
   remove(accountRef);
 };
 
-export const addRecord = (uid: string, payload: Record) => {
+export const firebaseAddRecord = async (payload: Omit<Record, "id">) => {
+  const uid = await getCurrentUserUid();
   const recordsRef = ref(database, `users/${uid}/records`);
-  push(recordsRef, payload);
+  return push(recordsRef, payload);
 };
 
-export const updateRecord = (uid: string, payload: Record) => {
+export const firebaseUpdateRecord = (uid: string, payload: Record) => {
   const recordRef = ref(database, `users/${uid}/records/${payload.id}`);
   set(recordRef, payload);
 };
 
-export const deleteRecord = (uid: string, payload: Record) => {
+export const firebaseDeleteRecord = (uid: string, payload: Record) => {
   const recordRef = ref(database, `users/${uid}/records/${payload.id}`);
   remove(recordRef);
 };

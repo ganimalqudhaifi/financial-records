b90b26170bfb669aa7f16f3f48cd2db264f90d7c
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2026-02-25 07:50:03 +0700

    chore(update): next dependency to next 16.x

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/app")) {
     const token = request.cookies.get("token");
 

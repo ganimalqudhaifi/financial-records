ce83c86bcd995726bfd72b3afd260c8946c9605c
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2026-03-26 12:14:13 +0700

    refactor(api/logout): Refactors logout API

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,25 +1,17 @@
 import { signOut } from "@/features/auth/services/auth.client";
-import cookie from "cookie";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export default async function logout(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+export async function POST() {
+  const response = NextResponse.next()
+
   try {
     await signOut();
 
-    const clearCookie = cookie.serialize("token", "", {
-      maxAge: -1, // Set the cookie to expire immediately
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      path: "/",
-    });
-    res.setHeader("Set-Cookie", clearCookie);
+    response.cookies.delete('token')
 
-    res.status(200).json({ message: "Logged out successfully" });
-  } catch (error) {
-    console.error("Logout error:", error);
-    res.status(500).json({ message: "Internal server error" });
+    return NextResponse.json({ message: "Logged out successfully" }, { status: 200 })
+  } catch (e) {
+    return NextResponse.json({ message: "Internal server error" }, { status: 500 })
   }
+
 }

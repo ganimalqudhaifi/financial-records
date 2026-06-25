eda9c294143a328a46a3a0d6b9274a32e679d939
ganimalqudhaifi <ganimalqudhaifi@gmail.com>
2026-06-25 21:23:05 +0700

    refactor(next.config): add image remote patterns for external sources

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
